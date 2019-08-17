cebdfa5227ee103464a840faafc4cb0e6ff94dea
jiandie012 <1026488168@qq.com>
2019-08-17 23:31:19 +0800

    none

---
+++ src/main/java/com/baidu/ai/aip/com/baidu/ai/aip/BodyAnalysis.java
@@ -0,0 +1,65 @@
+package com.baidu.ai.aip.com.baidu.ai.aip;
+
+import com.baidu.ai.aip.utils.Base64Util;
+import com.baidu.ai.aip.utils.FileUtil;
+import com.baidu.ai.aip.utils.HttpUtil;
+import com.baidu.aip.imageclassify.AipImageClassify;
+
+import java.net.URLEncoder;
+
+/**
+ * 人体关键点识别
+ */
+public class BodyAnalysis {
+    /**
+     * 重要提示代码中所需工具类
+     * FileUtil,Base64Util,HttpUtil,GsonUtils请从
+     * https://ai.baidu.com/file/658A35ABAB2D404FBF903F64D47C1F72
+     * https://ai.baidu.com/file/C8D81F3301E24D2892968F09AE1AD6E2
+     * https://ai.baidu.com/file/544D677F5D4E4F17B4122FBD60DB82B3
+     * https://ai.baidu.com/file/470B3ACCA3FE43788B5A963BF0B625F3
+     * 下载
+     */
+    public static String bodyAnalysis() {
+        // 请求url
+        String url = "https://aip.baidubce.com/rest/2.0/image-classify/v1/body_analysis";
+        try {
+            // 本地文件路径
+            String filePath = "/home/liugaojian/Desktop/xxx.png";
+            byte[] imgData = FileUtil.readFileByBytes(filePath);
+            String imgStr = Base64Util.encode(imgData);
+            String imgParam = URLEncoder.encode(imgStr, "UTF-8");
+
+            String param = "image=" + imgParam;
+
+            // 注意这里仅为了简化编码每一次请求都去获取access_token，线上环境access_token有过期时间， 客户端可自行缓存，过期后重新获取。
+            String accessToken = "access_token";
+
+            String result = HttpUtil.post(url, accessToken, param);
+            System.out.println(result);
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        BodyAnalysis.bodyAnalysis();
+    }
+    // 初始化一个AipImageClassify
+    AipImageClassify client = new AipImageClassify(APP_ID, API_KEY, SECRET_KEY);
+
+    // 可选：设置网络连接参数
+        client.setConnectionTimeoutInMillis(2000);
+        client.setSocketTimeoutInMillis(60000);
+
+
+    // 调用接口
+    String path = "/home/abc/Desktop/apple.jpg";
+
+    SampleImage sample=new SampleImage();
+        sample.image(client,path);
+
+
+}
